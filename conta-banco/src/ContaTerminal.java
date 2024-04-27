5f0740ac7ecdc661f11a13a1b4e4fa50edddf8cb
pedrovsfelix <pedrovitorsf@gmail.com>
2024-04-26 21:05:04 -0300

    Criando métodos scanner para input via terminal

--- conta-banco/src/ContaTerminal.java
+++ conta-banco/src/ContaTerminal.java
@@ -1,5 +1,24 @@
+import java.util.Locale;
+import java.util.Scanner;
+
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        // conhecer e importar a classe scanner
+        Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
+        // Exibir mensagems para o usuário
+        System.out.println("Digite o seu Nome e Sobrenome!");
+        String NomeCliente = scanner.next();
+
+        System.out.println("Por favor, digite o número da Agência!");
+        String Agencia = scanner.next();
+
+        System.out.println("Por favor, digite o número da Conta!");
+        int Numero = scanner.nextInt();
+
+        System.out.println("Por favor, o saldo da sua Conta!");
+        Double Saldo = scanner.nextDouble();
+
+        //Exibir mensagem conta criada
+        System.out.println("Olá " + NomeCliente + ", obrigado por criar uma conta em nosso banco, sua agência é" + Agencia + ", conta " + Numero +" e seu saldo " + Saldo + " já está disponível para saque.");
     }
 }
